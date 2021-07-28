d7bfce85913fbd82614be5e22ece1fb6eb8c8ac5
Oracle Public Cloud User <opc@instance-20200121-2104.sub01220003320.vcntestbox.oraclevcn.com>
2021-07-28 23:16:51 +0000

    Complete search (in/out)

--- darkbitcoingraph.py
+++ darkbitcoingraph.py
@@ -21,35 +21,6 @@ else:
 arr_top_senders = []
 arr_abuse = []
 
-#RECEIVED/SENT
-data = requests.get(url='https://hashxp.org/'+bitcoin_address)
-
-st = data.text.find('<tr><th>Received TX</th><td>')
-end = data.text[st+28:].find(' ')
-received = int(data.text[st+28:st+28+end])
-rel_data = data.text[end:]
-st = rel_data.find('<tr><th>Sent TX</th><td>')
-end = rel_data[st+24:].find(' ')
-sent = int(rel_data[st+24:st+24+end])
-print('Received / Sent: ',str(received),'/',str(sent),'\n')
-
-#TOP 5 SENDERS
-st = 0
-end = 0
-st_st = 0
-rel_data = data.text[st_st:]
-for i in range(5):
-	rel_data = rel_data[st_st:]
-	st = rel_data.find('tt class="btcaddr">')
-	end = rel_data[st:].find('</tt>')
-	obj = rel_data[st+19:st+end]
-	obj_count = int(rel_data[st+end+9:st+end+9+2])
-	if obj_count < 2:
-		break
-	st_st = st+end+9+2
-	arr_top_senders.append(obj+" "+str(obj_count))
-	obj_count = 0
-
 data = urllib.request.urlopen('https://blockchain.info/rawaddr/'+bitcoin_address)
 obj = json.loads(data.read())
 count = 0
@@ -57,29 +28,43 @@ addresses_already_requested = []
 
 #ABUSE
 for tx in obj['txs']:
-	tx_in_address = tx['inputs'][0]['prev_out']['addr']
+	type_tx = ['inputs', 'out']
+
+	for list_tytx in type_tx:
+		str_type_tx = 'inp' if list_tytx == 'inputs' else 'out'
+		for tytx in tx[list_tytx]:	
+			if list_tytx == 'inputs':
+				tx_in_address = tytx['prev_out']['addr']
+			elif list_tytx == 'out':
+				tx_in_address = tytx['addr']
+			else:
+				print('Invalid type transaction')
+				exit(1)
 	
-	if tx_in_address in addresses_already_requested:
-		continue
-	else:
-		addresses_already_requested.append(tx_in_address)
+			if tx_in_address in addresses_already_requested:
+				continue
+			else:
+				addresses_already_requested.append(tx_in_address)
 
-	data_abuse = requests.get('https://www.bitcoinabuse.com/api/reports/check?address='+tx_in_address+'&api_token='+API_TOKEN)
-	count += 1
+			data_abuse = requests.get('https://www.bitcoinabuse.com/api/reports/check?address='+tx_in_address+'&api_token='+API_TOKEN)
+			count += 1
 
-	if (count > 1 and count % 30 == 0):
-		time.sleep(60)
+			if (count > 1 and count % 30 == 0):
+				time.sleep(60)
 
-	try:
-		obj_abuse = json.loads(data_abuse.text)
-	except:
-		print('Error generating JSON from: '+data_abuse.text)
+			try:
+				obj_abuse = json.loads(data_abuse.text)
+			except:
+				print('Error generating JSON from: '+data_abuse.text)
 
-	if (obj_abuse['count'] > 0):
-		arr_abuse.append(obj_abuse['address']+' '+str(obj_abuse['count']))
+			if (obj_abuse['count'] > 0):
+				#arr_abuse.append(obj_abuse['address']+' '+str(obj_abuse['count']))
+				print(str_type_tx,')',obj_abuse['address'],str(obj_abuse['count']))
+		print(list_tytx,'done')
+	break
 
-print('======TOP 5 SENDERS======')
-print('\n'.join(arr_top_senders))
+#print('======TOP 5 SENDERS======')
+#print('\n'.join(arr_top_senders))
 
-print('\n======ABUSE TRANSACTIONS======')
-print('\n'.join(arr_abuse))
+#print('\n======ABUSE TRANSACTIONS======')
+#print('\n'.join(arr_abuse))
