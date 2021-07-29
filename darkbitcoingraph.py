2dfc8108a602709cddb8bf859635ef729fe77ef1
Oracle Public Cloud User <opc@instance-20200121-2104.sub01220003320.vcntestbox.oraclevcn.com>
2021-07-29 00:17:32 +0000

    Top senders with raw data

--- darkbitcoingraph.py
+++ darkbitcoingraph.py
@@ -25,6 +25,7 @@ data = urllib.request.urlopen('https://blockchain.info/rawaddr/'+bitcoin_address
 obj = json.loads(data.read())
 count = 0
 addresses_already_requested = []
+count_already_requested = []
 
 #ABUSE
 for tx in obj['txs']:
@@ -32,19 +33,24 @@ for tx in obj['txs']:
 
 	for list_tytx in type_tx:
 		str_type_tx = 'inp' if list_tytx == 'inputs' else 'out'
+
 		for tytx in tx[list_tytx]:	
 			if list_tytx == 'inputs':
 				tx_in_address = tytx['prev_out']['addr']
 			elif list_tytx == 'out':
+				continue
 				tx_in_address = tytx['addr']
 			else:
-				print('Invalid type transaction')
+				print('Invalid transaction type')
 				exit(1)
-	
+		
 			if tx_in_address in addresses_already_requested:
+				pos_count = addresses_already_requested.index(tx_in_address)
+				count_already_requested[pos_count] += 1
 				continue
 			else:
 				addresses_already_requested.append(tx_in_address)
+				count_already_requested.append(1)
 
 			data_abuse = requests.get('https://www.bitcoinabuse.com/api/reports/check?address='+tx_in_address+'&api_token='+API_TOKEN)
 			count += 1
@@ -60,11 +66,24 @@ for tx in obj['txs']:
 			if (obj_abuse['count'] > 0):
 				#arr_abuse.append(obj_abuse['address']+' '+str(obj_abuse['count']))
 				print(str_type_tx,')',obj_abuse['address'],str(obj_abuse['count']))
-		print(list_tytx,'done')
-	break
+		#end for tx[list_tytx]
+	#end for type_tx
+#end for obj['txs']
 
-#print('======TOP 5 SENDERS======')
-#print('\n'.join(arr_top_senders))
+sorted_count = (sorted(count_already_requested)[::-1])[0:5]
+
+count = 0
+count_top_senders = 0
+
+print('======TOP 5 SENDERS======')
+for i in addresses_already_requested:
+	cc = count_already_requested[count]
+	count += 1
+	if cc > 1 and cc in sorted_count:
+		print(i,str(cc))
+		count_top_senders += 1
+	if count_top_senders == 5:
+		break
 
 #print('\n======ABUSE TRANSACTIONS======')
 #print('\n'.join(arr_abuse))
