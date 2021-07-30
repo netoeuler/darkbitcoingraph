caa14b196afc8661e429c36f81be590c3b9782bd
Oracle Public Cloud User <opc@instance-20200121-2104.sub01220003320.vcntestbox.oraclevcn.com>
2021-07-30 00:59:52 +0000

    Write/Read files

--- darkbitcoingraph.py
+++ darkbitcoingraph.py
@@ -4,6 +4,7 @@ import json
 import time
 import sys
 import re
+import os.path
 
 API_TOKEN = ''
 
@@ -20,20 +21,59 @@ else:
 
 arr_top_senders = []
 arr_abuse = []
+arr_relayed_ips = []
 
 data = urllib.request.urlopen('https://blockchain.info/rawaddr/'+bitcoin_address)
 obj = json.loads(data.read())
 
 count = 0
+
+if os.path.exists('output/count/'+bitcoin_address):
+	file_addr_count = open('output/count/'+bitcoin_address,'r')
+	file_addr_line = file_addr_count.readline()
+	if len(file_addr_line) > 0:
+		try:
+			count = int(file_addr_line.replace('\n',''))
+			#print('Starting with count',str(count))
+		except:
+			print('Error with count')
+			exit(1)
+		finally:
+			file_addr_count.close()
+
+count_abuse = 0
+count_count = 0
 received = 0
 sent = 0
 addresses_already_requested = []
 count_already_requested = []
-arr_relayed_ips = []
 
-#ABUSE
+num_obj = len(obj['txs'])
+compl_text_obj = '.'
+if count > 0:
+	num_obj = num_obj - count
+	compl_text_obj = 'still missing.'
+	count_count = count
+
+resp = 0
+while resp not in ['Y','N']:
+	print('There are',num_obj,'transactions',compl_text_obj,'Do you wanna proceed? [y/n]',end=' ')
+	resp = input().upper()
+
+if resp == 'N':
+	exit(0)
+
+file_addr = open('output/'+bitcoin_address,'a')
+
 for tx in obj['txs']:
+	if count_count > 0:
+		count_count -= 1
+		continue
+	
 	#type_tx = ['inputs', 'out']
+	file_addr_count = open('output/count/'+bitcoin_address,'w')
+	file_addr_count.write(str(count))
+	file_addr_count.close()
 
 	if tx['relayed_by'] not in arr_relayed_ips:
 		arr_relayed_ips.append(tx['relayed_by'])
@@ -60,6 +100,8 @@ for tx in obj['txs']:
 				print('Invalid transaction type')
 				exit(1)
 		
+			count += 1
+
 			if tx_in_address in addresses_already_requested:
 				pos_count = addresses_already_requested.index(tx_in_address)
 				count_already_requested[pos_count] += 1
@@ -69,9 +111,9 @@ for tx in obj['txs']:
 				count_already_requested.append(1)
 	
 			data_abuse = requests.get('https://www.bitcoinabuse.com/api/reports/check?address='+tx_in_address+'&api_token='+API_TOKEN)
-			count += 1
+			count_abuse += 1
 
-			if (count > 1 and count % 30 == 0):
+			if (count_abuse > 1 and count_abuse % 30 == 0):
 				time.sleep(60)
 
 			try:
@@ -81,11 +123,13 @@ for tx in obj['txs']:
 
 			if (obj_abuse['count'] > 0):
 				arr_abuse.append(str_type_tx+' '+obj_abuse['address']+' '+str(obj_abuse['count']))
-				#print(str_type_tx,')',obj_abuse['address'],str(obj_abuse['count']))
+				file_addr.write(obj_abuse['address']+'\n')
+				#print(str(count),str_type_tx,')',obj_abuse['address'],str(obj_abuse['count']))
 		#end for tx[list_tytx]
 		print('|',sep=" ",end='',flush=True)
 	#end for type_tx
 #end for obj['txs']
+file_addr.close()
 
 print('\n\nReceived:',received,'/ Sent:',sent)
 #print('Relayed IPs:',' '.join(arr_relayed_ips))
